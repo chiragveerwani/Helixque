9eaf217cefa63dd2c676393319e3dc9e802c2dcb
ANUKOOL <anukoolbhul324@gmail.com>
2025-10-14 13:08:11 +0530

    pushing here

--- frontend/components/RTC/Room.tsx
+++ frontend/components/RTC/Room.tsx
@@ -828,7 +828,7 @@ export default function Room({
             bg-neutral-950 backdrop-blur-sm transition-transform duration-300
              z-30 h-full
             ${
-              showChat ? "translate-x-0" : "translate-x-full hidden" 
+              showChat ? "translate-x-0" :"translate-x-full hidden" 
             }
           `}
         >
